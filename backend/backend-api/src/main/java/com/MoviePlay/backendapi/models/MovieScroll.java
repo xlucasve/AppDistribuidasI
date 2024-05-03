67f602ab18bd31d7a710b008d832d88e58a9f5e7
Lucas <lucasvonelm@gmail.com>
2024-05-03 18:09:33 -0300

    Fixed project for deploy

--- backend/backend-api/src/main/java/com/MoviePlay/backendapi/models/MovieScroll.java
+++ backend/backend-api/src/main/java/com/MoviePlay/backendapi/models/MovieScroll.java
@@ -1,21 +1,47 @@
 package com.MoviePlay.backendapi.models;
 
 import com.MoviePlay.backendapi.dtos.responses.ResponseMovieInScroll;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class MovieScroll {
 
     private List<ResponseMovieInScroll> sidescroll = new ArrayList<>();
     private Integer count;
     private String title;
+
+    public MovieScroll() {
+    }
+
+    public MovieScroll(List<ResponseMovieInScroll> sidescroll, Integer count, String title) {
+        this.sidescroll = sidescroll;
+        this.count = count;
+        this.title = title;
+    }
+
+    public List<ResponseMovieInScroll> getSidescroll() {
+        return sidescroll;
+    }
+
+    public void setSidescroll(List<ResponseMovieInScroll> sidescroll) {
+        this.sidescroll = sidescroll;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
