9b8e47940b2a4c1cb2826d06441d4a0fd9db3597
Lucas <lucasvonelm@gmail.com>
2024-05-16 15:04:32 -0300

    feat: improved search method
    It now searches if it contains the input, instead of searching if the input exactly matches.

--- backend/backend-api/src/main/java/com/MoviePlay/backendapi/repositories/ActorRepository.java
+++ backend/backend-api/src/main/java/com/MoviePlay/backendapi/repositories/ActorRepository.java
@@ -4,11 +4,14 @@ import com.MoviePlay.backendapi.entities.Actor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ActorRepository extends JpaRepository<Actor, Long> {
     Optional<Actor> findByName(String name);
+
+    @Query("SELECT a FROM Actor a WHERE a.name LIKE %?1%")
     Page<Actor> findAllByName(String name, Pageable pageable);
 }
--- backend/backend-api/src/main/java/com/MoviePlay/backendapi/repositories/MovieRepository.java
+++ backend/backend-api/src/main/java/com/MoviePlay/backendapi/repositories/MovieRepository.java
@@ -4,6 +4,7 @@ import com.MoviePlay.backendapi.entities.Movie;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,5 +15,6 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     Page<Movie> findAllByOrderByReleaseDateDesc(Pageable pageable);
     Optional<Movie> findByTitle(String title);
 
+    @Query("SELECT m FROM Movie m WHERE m.title LIKE %?1%")
     Page<Movie> findAllByTitle(String input, Pageable pageable);
 }
