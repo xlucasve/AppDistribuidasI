51e84d60d1bdff671d7bf25c8e4ecdeeef88c519
Lucas <lucasvonelm@gmail.com>
2024-05-27 09:56:48 -0300

    Added Google Sign in

--- frontend/MoviePlayApp/src/Navigation/Navigation.js
+++ frontend/MoviePlayApp/src/Navigation/Navigation.js
@@ -1,97 +1,98 @@
-import React from "react";
-import { Text, Image, StyleSheet, View, Pressable } from "react-native";
-import { DarkTheme, NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import {Text, Image, StyleSheet, View, Pressable} from 'react-native';
+import {DarkTheme, NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../screens/Home';
 import Profile from '../screens/Profile/Profile';
 import Search from '../screens/Search';
 import Login from '../screens/Login';
 import Logo from '../assets/images/logo.svg';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { HomeOptions, ProfileOptions } from "./HeaderOptions";
-
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {HomeOptions, ProfileOptions} from './HeaderOptions';
 
 const Stack = createNativeStackNavigator();
 
 const StackGroup = () => {
-    return (
-        <Stack.Navigator initialRouteName='Login'>
+  return (
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={Login} />
 
-        <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Home" component={Home} />
 
-        <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Profile" component={Profile} />
 
-        <Stack.Screen name="Profile" component={Profile} />
-
-        <Stack.Screen name="Search" component={Search} />
-
-      </Stack.Navigator> 
-    )
-  }
-
-
+      <Stack.Screen name="Search" component={Search} />
+    </Stack.Navigator>
+  );
+};
 
 // BottomTabNavigator
 const Tab = createBottomTabNavigator();
 
 const TabGroup = () => {
-    return (
-        <Tab.Navigator
-        initialRouteName="Mi Perfil"
-        screenOptions={({route, navigation}) => ({
-            tabBarIcon: ({color, focused}) => {
-                let iconName;
-                if (route.name === 'Inicio') {
-                    iconName = 'home'
-                } else if (route.name === 'Favoritos') {
-                    iconName = 'star' 
-                } else if (route.name === 'Mi Perfil') {
-                    iconName = 'person'
-                }
-                return <Ionicons name={iconName} size={hp('4.2%')} color={color} />
-            },
-            tabBarActiveTintColor: '#D51D53',
-            tabBarInactiveTintColor: '#FAFAFA',
-            tabBarStyle: {
-                backgroundColor: '#192941',
-                borderTopWidth: 0,
-              }
-            
+  return (
+    <Tab.Navigator
+      initialRouteName="Mi Perfil"
+      screenOptions={({route, navigation}) => ({
+        tabBarIcon: ({color, focused}) => {
+          let iconName;
+          if (route.name === 'Inicio') {
+            iconName = 'home';
+          } else if (route.name === 'Favoritos') {
+            iconName = 'star';
+          } else if (route.name === 'Mi Perfil') {
+            iconName = 'person';
+          }
+          return <Ionicons name={iconName} size={hp('4.2%')} color={color} />;
+        },
+        tabBarActiveTintColor: '#D51D53',
+        tabBarInactiveTintColor: '#FAFAFA',
+        tabBarStyle: {
+          backgroundColor: '#192941',
+          borderTopWidth: 0,
+        },
+      })}>
+      <Tab.Screen name="Inicio" component={Home} options={HomeOptions} />
+      <Tab.Screen
+        name="Favoritos"
+        component={Home}
+        options={{
+          tabBarButton: props => <Pressable {...props} disabled={true} />,
+        }}
+      />
+      <Tab.Screen
+        name="Mi Perfil"
+        component={Profile}
+        options={ProfileOptions}
+      />
+      <Tab.Screen name="Sign in" component={Login} />
+    </Tab.Navigator>
+  );
+};
 
-    })}
-        >
-            <Tab.Screen name="Inicio" component={Home} options={HomeOptions} />
-            <Tab.Screen name="Favoritos" component={Home} options={{
-                  tabBarButton: (props) => <Pressable {...props} disabled={true} />
-                }}/>
-            <Tab.Screen name="Mi Perfil" component={Profile} options={ProfileOptions} />
-        </Tab.Navigator>
-    )
-}
-
-export default function Navigation({ navigation }) {
-    return (
+export default function Navigation({navigation}) {
+  return (
     <NavigationContainer>
-    
-
       <TabGroup />
       {/* <StackGroup /> */}
     </NavigationContainer>
-    )
+  );
 }
 
-
 const styles = StyleSheet.create({
-    logoSize: {
-      width: wp('12%'),
-      height: hp('5.5%')
-    },
-    leftHeader: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderRadius: 100,
-      overflow: 'hidden',
-    }
-  })
+  logoSize: {
+    width: wp('12%'),
+    height: hp('5.5%'),
+  },
+  leftHeader: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 100,
+    overflow: 'hidden',
+  },
+});
--- frontend/MoviePlayApp/src/screens/Login.jsx
+++ frontend/MoviePlayApp/src/screens/Login.jsx
@@ -1,86 +1,105 @@
-import * as React from "react";
-import { TouchableOpacity, StyleSheet, Text, View } from "react-native";
-import Logo from "../assets/images/logo.svg";
-import GoogleLogo from "../assets/images/login_btnGoogle.svg";
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import * as React from 'react';
+import {TouchableOpacity, StyleSheet, Text, View} from 'react-native';
+import Logo from '../assets/images/logo.svg';
+import GoogleLogo from '../assets/images/login_btnGoogle.svg';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
+export default function Login({navigation}) {
+  const onGoogleButtonPress = async () => {
+    try {
+      GoogleSignin.configure({
+        webClientId:
+          '568362713599-sjg142r6t6o2nq44nhrv11lnq1g6s37i.apps.googleusercontent.com',
+      });
+      await GoogleSignin.hasPlayServices();
+      const {user} = await GoogleSignin.signIn();
+      console.log(user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-export default function Login({ navigation }) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.rectangleContainer}>
-                <View style={styles.logoContainer}>
-                    <Logo
-                        width={styles.logoContainer.width}
-                        height={styles.logoContainer.height} />
-                </View>
-                <View style={styles.SignInContainer}>
-                    <Text style={styles.signInText}>Sign In</Text>
-                    <TouchableOpacity style={styles.signButton} onPress={() => navigation.navigate('Home')} >
-                        <GoogleLogo />
-                        <Text style={styles.textButton}>Sign in with Google</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.rectangleContainer}>
+        <View style={styles.logoContainer}>
+          <Logo
+            width={styles.logoContainer.width}
+            height={styles.logoContainer.height}
+          />
         </View>
-    );
+        <View style={styles.SignInContainer}>
+          <Text style={styles.signInText}>Sign In</Text>
+          <TouchableOpacity
+            style={styles.signButton}
+            onPress={() => onGoogleButtonPress()}>
+            <GoogleLogo />
+            <Text style={styles.textButton}>Sign in with Google</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#01152D',
-    },
-    rectangleContainer: {
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'start',
-        backgroundColor: '#192941',
-        height: hp('75%'),
-        width: wp('86%'),
-        borderRadius: 40,
-        elevation: 10, // Añadir sombra para Android
-        shadowColor: '#000', // Añadir sombra para iOS
-        shadowOffset: { width: 0, height: 2 }, // Añadir sombra para iOS
-        shadowOpacity: 0.25, // Añadir sombra para iOS
-        shadowRadius: 3.84, // Añadir sombra para iOS
-
-    },
-    logoContainer: {
-        width: wp('50%'),
-        height: hp('25%'),
-        borderRadius: 100,
-        marginTop: hp('6.25%'),
-        overflow: 'hidden',
-    },
-    SignInContainer: {
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginVertical: hp('7.5%'),
-    },
-    signInText: {
-        color: '#fff',
-        fontSize: hp('4.5%'),
-        fontWeight: 'bold',
-        marginBottom: hp('3.5%'),
-    },
-    signButton: {
-        marginVertical: hp('3.75%'),
-        flexDirection: 'row',
-        backgroundColor: '#FAFAFA',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 30,
-        width: wp('58%'),
-        height: hp('7.5%'),
-    },
-    textButton: {
-        marginLeft: wp('3%'),
-        color: 'black',
-        fontWeight: 'medium',
-        fontSize: hp('1.875%'),
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#01152D',
+  },
+  rectangleContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'start',
+    backgroundColor: '#192941',
+    height: hp('75%'),
+    width: wp('86%'),
+    borderRadius: 40,
+    elevation: 10, // Añadir sombra para Android
+    shadowColor: '#000', // Añadir sombra para iOS
+    shadowOffset: {width: 0, height: 2}, // Añadir sombra para iOS
+    shadowOpacity: 0.25, // Añadir sombra para iOS
+    shadowRadius: 3.84, // Añadir sombra para iOS
+  },
+  logoContainer: {
+    width: wp('50%'),
+    height: hp('25%'),
+    borderRadius: 100,
+    marginTop: hp('6.25%'),
+    overflow: 'hidden',
+  },
+  SignInContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: hp('7.5%'),
+  },
+  signInText: {
+    color: '#fff',
+    fontSize: hp('4.5%'),
+    fontWeight: 'bold',
+    marginBottom: hp('3.5%'),
+  },
+  signButton: {
+    marginVertical: hp('3.75%'),
+    flexDirection: 'row',
+    backgroundColor: '#FAFAFA',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    width: wp('58%'),
+    height: hp('7.5%'),
+  },
+  textButton: {
+    marginLeft: wp('3%'),
+    color: 'black',
+    fontWeight: 'medium',
+    fontSize: hp('1.875%'),
+  },
 });
