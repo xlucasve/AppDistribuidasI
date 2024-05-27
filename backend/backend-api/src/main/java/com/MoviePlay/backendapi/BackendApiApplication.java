cb6550ed589a636a146e6c93fa41f525f881d639
Lucas <lucasvonelm@gmail.com>
2024-05-27 20:02:23 -0300

    MovedKeepAlive

--- backend/backend-api/src/main/java/com/MoviePlay/backendapi/BackendApiApplication.java
+++ backend/backend-api/src/main/java/com/MoviePlay/backendapi/BackendApiApplication.java
@@ -8,23 +8,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
-@EnableScheduling
+
 public class BackendApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApiApplication.class, args);
 	}
 
-	@Scheduled(fixedDelay = 1000*60)
-	public void keepAlive(){
-		try {
-			Unirest.setTimeouts(0, 0);
-			HttpResponse<String> response = Unirest.get("https://keepalive-h1q1.onrender.com/api/")
-					.asString();
-			System.out.println("KEEP ALIVE RESPONSE: " + response.getBody());
-			System.out.println("Called");
-		} catch (Exception e) {
-			System.out.println("ERROR CALLIN KEEP ALIVE: " + e.getMessage());
-		}
-	}
 }
