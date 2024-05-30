a296f44d62c5d58f632bbfd587f33de957e17264
Lucas <lucasvonelm@gmail.com>
2024-05-30 12:42:32 -0300

    Added loading indicator to Home Screen

--- frontend/MoviePlayApp/src/screens/Home.jsx
+++ frontend/MoviePlayApp/src/screens/Home.jsx
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   TextInput,
   FlatList,
+  ActivityIndicator,
 } from 'react-native';
 import {View} from 'react-native';
 import Header from '../components/Header';
@@ -17,6 +18,7 @@ export default function Home({navigation}) {
   const textRef = React.useRef(null);
   const [isEditable, setIsEditable] = React.useState(false);
 
+  const [isLoading, setIsLoading] = React.useState(true);
   const [movieData, setMovieData] = React.useState({});
 
   const logTokeN = async () => {
@@ -28,20 +30,28 @@ export default function Home({navigation}) {
     const getData = async () => {
       let response = await movieService.getHomeData();
       setMovieData(response);
+      setIsLoading(false);
     };
     getData();
   }, []);
 
   const Item = items => (
-    <View>
-      <Image source={{uri: items.image}} style={{width: 400, height: 400}} />
+    <View style={styles.carouselItemContainer}>
+      <Image
+        source={{uri: items.image}}
+        style={{
+          width: 51,
+          height: 51,
+          resizeMode: 'contain',
+        }}
+      />
       <Text>{items.title}</Text>
     </View>
   );
 
   const BigMovieCarousel = () => {
     return (
-      <View>
+      <View style={styles.fullHomeContainer}>
         <FlatList
           data={movieData.bigMovies.moviesData}
           renderItem={({item}) => (
@@ -49,6 +59,7 @@ export default function Home({navigation}) {
           )}
           keyExtractor={item => item.movieId}
           horizontal
+          pagingEnabled
         />
       </View>
     );
@@ -56,23 +67,18 @@ export default function Home({navigation}) {
 
   return (
     <View style={backDefaultContainerStyle}>
-      <BigMovieCarousel />
+      {isLoading ? <ActivityIndicator /> : <BigMovieCarousel />}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  temporalButton1: {
-    backgroundColor: 'red',
-    height: 50,
-    alignContent: 'center',
-    justifyContent: 'center',
+  fullHomeContainer: {
+    flex: 1,
   },
 
-  temporalButton2: {
-    backgroundColor: 'green',
-    height: 50,
-    alignContent: 'center',
-    justifyContent: 'center',
+  carouselItemContainer: {
+    flex: 0.9,
+    marginRight: 100,
   },
 });
