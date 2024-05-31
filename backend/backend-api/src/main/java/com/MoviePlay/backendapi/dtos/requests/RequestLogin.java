4897d88b05586fa65746561b6b6614be70d8b692
Nacho-93 <ignacioindurainusa@hotmail.com>
2024-05-31 19:12:26 -0300

    random nickname generator

--- backend/backend-api/src/main/java/com/MoviePlay/backendapi/dtos/requests/RequestLogin.java
+++ backend/backend-api/src/main/java/com/MoviePlay/backendapi/dtos/requests/RequestLogin.java
@@ -1,4 +1,4 @@
 package com.MoviePlay.backendapi.dtos.requests;
 
-public record RequestLogin(String userEmail, String realName, String nickname, String profilePictureLink) {
+public record RequestLogin(String userEmail, String realName, String profilePictureLink) {
 }
