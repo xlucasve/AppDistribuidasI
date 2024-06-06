ab24e2b0fdc62ae433b708c2a3560e02b1c506e7
Lucas <lucasvonelm@gmail.com>
2024-06-06 14:17:17 -0300

    Added search result flatlist

--- frontend/MoviePlayApp/src/components/MovieCard.jsx
+++ frontend/MoviePlayApp/src/components/MovieCard.jsx
@@ -1,149 +1,153 @@
-import React from "react";
-import { View, Text, Image, StyleSheet, Pressable } from "react-native";
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import FavoriteMovie_false from "../assets/images/favoriteMovie_false.svg";
-import FavoriteMovie_true from "../assets/images/favoriteMovie_true.svg";
-import RatingStar from "../assets/images/ratingStar.svg";
+import React from 'react';
+import {View, Text, Image, StyleSheet, Pressable} from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import FavoriteMovie_false from '../assets/images/favoriteMovie_false.svg';
+import FavoriteMovie_true from '../assets/images/favoriteMovie_true.svg';
+import RatingStar from '../assets/images/ratingStar.svg';
 
-export default function MovieCard({ movie }) {
+export default function MovieCard({movie}) {
+  const [isFavorite, setIsFavorite] = React.useState(false);
 
-    const [isFavorite, setIsFavorite] = React.useState(false);
+  const genresList = movie.genres.map(genre => {
+    return <GenreCard genre={genre.name} key={genre.genreId} />;
+  });
 
-    const genresList = movie.genres.map(genre => {
-        return <GenreCard genre={genre.name} key={genre.genreId} />
-    });
+  const ratingStarsList = [];
 
-    const ratingStarsList = [];
-
-    for (let i = 1; i <= 5; i++) {
-        ratingStarsList
-            .push(
-                <RatingStar
-                    key={i}
-                    style={styles.body.ratingStar}
-                    fill={i < movie.rating ? '#FFD700' : '#03152D'}
-
-                    width={styles.body.ratingStar.width}
-                    height={styles.body.ratingStar.height}
-                />);
-    }
-
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.poster.container}>
-                <Image source={movie.posterImageLink} alt={movie.title} style={styles.poster.image} />
-            </View>
-
-            <View style={styles.body.container}>
-                <Text style={styles.body.title}>{movie.title}</Text>
-                <View style={styles.body.rating.container}>{ratingStarsList}
-                    <Text style={styles.body.rating.text}>{movie.rating}</Text></View>
-                <View style={styles.body.genre}>{genresList}</View>
-                <Pressable style={styles.body.favorite} onPress={() => setIsFavorite(!isFavorite)}>
-                    {!isFavorite
-                        ? <FavoriteMovie_false></FavoriteMovie_false>
-                        : <FavoriteMovie_true></FavoriteMovie_true>}
-                </Pressable>
-            </View>
-        </View>
+  for (let i = 1; i <= 5; i++) {
+    ratingStarsList.push(
+      <RatingStar
+        key={i}
+        style={styles.body.ratingStar}
+        fill={i < movie.rating ? '#FFD700' : '#03152D'}
+        width={styles.body.ratingStar.width}
+        height={styles.body.ratingStar.height}
+      />,
     );
-}
+  }
 
+  return (
+    <View style={styles.container}>
+      <View style={styles.poster.container}>
+        <Image
+          source={{uri: movie.posterImageLink}}
+          alt={movie.title}
+          style={styles.poster.image}
+        />
+      </View>
+
+      <View style={styles.body.container}>
+        <Text style={styles.body.title}>{movie.title}</Text>
+        <View style={styles.body.rating.container}>
+          {ratingStarsList}
+          <Text style={styles.body.rating.text}>{movie.rating}</Text>
+        </View>
+        <View style={styles.body.genre}>{genresList}</View>
+        <Pressable
+          style={styles.body.favorite}
+          onPress={() => setIsFavorite(!isFavorite)}>
+          {!isFavorite ? (
+            <FavoriteMovie_false></FavoriteMovie_false>
+          ) : (
+            <FavoriteMovie_true></FavoriteMovie_true>
+          )}
+        </Pressable>
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
+  container: {
+    width: wp('90%'),
+    height: hp('17.5%'),
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginVertical: hp('1%'),
+  },
+  poster: {
     container: {
-        width: wp('90%'),
-        height: hp('17.5%'),
-        alignItems: 'center',
-        justifyContent: 'center',
+      width: wp('25%'),
+      height: hp('17%'),
+      borderRadius: 10,
+      overflow: 'hidden',
+      borderRadius: 10,
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 4,
+    },
+    image: {
+      width: '100%',
+      height: '100%',
+    },
+  },
+
+  body: {
+    container: {
+      width: wp('65%'),
+      height: hp('17%'),
+      justifyContent: 'space-around',
+      marginLeft: wp('4%'),
+      top: hp('-1%'),
+    },
+    title: {
+      color: '#FAFAFA',
+      fontSize: hp('3%'),
+      fontWeight: 'bold',
+    },
+    rating: {
+      container: {
         flexDirection: 'row',
-        marginVertical: hp('1%'),
+        alignItems: 'center',
+        marginBottom: hp('1%'),
+      },
+      text: {
+        color: '#FAFAFA',
+        fontSize: hp('2.5%'),
+        marginLeft: wp('1%'),
+      },
     },
-    poster: {
-        container: {
-            width: wp('25%'),
-            height: hp('17%'),
-            borderRadius: 10,
-            overflow: 'hidden',
-            borderRadius: 10,
-            shadowColor: '#000',
-            shadowOffset: {
-                width: 0,
-                height: 2,
-            },
-            shadowOpacity: 0.25,
-            shadowRadius: 3.84,
-            elevation: 4,
-
-        },
-        image: {
-            width: '100%',
-            height: '100%',
-        },
+    ratingStar: {
+      width: wp('5%'),
+      height: hp('4%'),
+      marginRight: wp('1%'),
     },
 
-    body: {
-        container: {
-            width: wp('65%'),
-            height: hp('17%'),
-            justifyContent: 'space-around',
-            marginLeft: wp('4%'),
-            top: hp('-1%'),
-        },
-        title: {
-            color: '#FAFAFA',
-            fontSize: hp('3%'),
-            fontWeight: "bold",
-        },
-        rating: {
-            container: {
-                flexDirection: 'row',
-                alignItems: 'center',
-                marginBottom: hp('1%'),
-            },
-            text: {
-                color: '#FAFAFA',
-                fontSize: hp('2.5%'),
-                marginLeft: wp('1%'),
-            },
-        },
-        ratingStar: {
-            width: wp('5%'),
-            height: hp('4%'),
-            marginRight: wp('1%'),
-        },
-
-        genre: {
-            flexDirection: 'row',
-            container: {
-                marginRight: wp('4%'),
-                marginBottom: hp('1%'),
-                borderRadius: 5,
-                borderWidth: 1,
-                borderColor: '#DADADA',
-                padding: 2,
-
-            },
-            text: {
-                color: '#FAFAFA',
-                fontSize: hp('1.5%'),
-            },
-        },
-
-        favorite: {
-            bottom: hp('-.8%'),
-        },
-
-
+    genre: {
+      flexDirection: 'row',
+      container: {
+        marginRight: wp('4%'),
+        marginBottom: hp('1%'),
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#DADADA',
+        padding: 2,
+      },
+      text: {
+        color: '#FAFAFA',
+        fontSize: hp('1.5%'),
+      },
     },
+
+    favorite: {
+      bottom: hp('-.8%'),
+    },
+  },
 });
 
-
-const GenreCard = ({ genre }) => {
-    return (
-        <View style={styles.body.genre.container}>
-            <Text style={styles.body.genre.text}>{genre}</Text>
-        </View>
-    );
-}
+const GenreCard = ({genre}) => {
+  return (
+    <View style={styles.body.genre.container}>
+      <Text style={styles.body.genre.text}>{genre}</Text>
+    </View>
+  );
+};
--- frontend/MoviePlayApp/src/screens/Search.jsx
+++ frontend/MoviePlayApp/src/screens/Search.jsx
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   TextInput,
   TouchableOpacity,
+  FlatList,
 } from 'react-native';
 import {View} from 'react-native';
 import SearchIcon from '../assets/images/search_btn.svg';
@@ -53,22 +54,25 @@ export default function Search({navigation}) {
   };
 
   const handleSearch = async () => {
+    if (!searchInput.trim().length) {
+      return;
+    }
+
     setIsLoading(true);
-    console.log(searchInput);
     const response = await movieService.searchMovies(
-      searchInput,
+      searchInput.trimStart(),
       'DESC',
       'DATE',
     );
     setMovieData(response);
     setIsLoading(false);
-    console.log(response);
   };
 
   movie_test_data = {
     movieId: 1,
     title: 'Dune: Part 2',
-    posterImageLink: movieImageTest,
+    posterImageLink:
+      'https://image.tmdb.org/t/p/original/gzb6P78zeFTnv9eoFYnaJ2YrZ5q.jpg',
     rating: 4.2,
     genres: [
       {
@@ -89,9 +93,11 @@ export default function Search({navigation}) {
   const SearchView = () => {
     return (
       <View style={styles.container}>
-        <Text>{searchInput}</Text>
-        <MovieCard movie={movie_test_data} />
-        <MovieCard movie={movie_test_data} />
+        <FlatList
+          data={movieData.movies}
+          renderItem={({item}) => <MovieCard movie={item} />}
+          keyExtractor={item => item.movieId}
+        />
       </View>
     );
   };
@@ -132,136 +138,3 @@ const styles = StyleSheet.create({
     color: 'black',
   },
 });
-
-/* import React, {useState} from 'react';
-import {
-  Text,
-  Image,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-  FlatList,
-} from 'react-native';
-import {View} from 'react-native';
-import {backDefaultContainerStyle} from '../styles/GlobalStyles';
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from 'react-native-responsive-screen';
-import SearchIcon from '../assets/images/search_btn.svg';
-import movieService from '../services/moviesService';
-import LoadingPage from '../components/LoadingPage';
-
-export default function Search({navigation}) {
-  const [searchInput, setSearchInput] = useState('');
-  const [movieData, setMovieData] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleChangeInput = input => {
-    setSearchInput(input);
-  };
-
-  const handleSearch = async () => {
-    setIsLoading(true);
-    const response = await movieService.searchMovies(
-      searchInput,
-      'DESC',
-      'DATE',
-    );
-    setMovieData(response);
-    setIsLoading(false);
-  };
-  React.useEffect(() => {
-    navigation.setOptions({
-      headerTitle: () => (
-        <View style={styles.headerContainer}>
-          <View style={styles.searchIcon}>
-            <TouchableOpacity onPress={() => handleSearch()}>
-              <SearchIcon width={35} height={35} />
-            </TouchableOpacity>
-          </View>
-          <View style={styles.inputContainer}>
-            <TextInput
-              style={styles.inputText}
-              defaultValue={searchInput}
-              onChangeText={value => handleChangeInput(value)}
-              placeholder="Hoy quiero buscar..."
-              onSubmitEditing={() => handleSearch()}
-            />
-          </View>
-        </View>
-      ),
-      headerRight: () => (
-        <View>
-          <SearchIcon width={35} height={35} />
-        </View>
-      ),
-    });
-  });
-
-  const MovieItem = items => {
-    return (
-      <View>
-        <Image
-          source={{uri: items.image}}
-          style={{
-            borderRadius: 4,
-            resizeMode: 'cover',
-            width: 150,
-            height: 200,
-          }}
-        />
-      </View>
-    );
-  };
-
-  const SearchItems = () => {
-    return (
-      <View>
-        <Text>Peliculas</Text>
-        <FlatList
-          data={movieData.movies}
-          renderItem={({item}) => <MovieItem image={item.posterImageLink} />}
-          keyExtractor={item => item.movieId}
-          contentContainerStyle={{
-            paddingRight: 20,
-            paddingLeft: 20,
-            paddingTop: 30,
-          }}
-          ItemSeparatorComponent={() => <View style={{height: 30}} />}
-        />
-      </View>
-    );
-  };
-
-  return (
-    <View style={backDefaultContainerStyle}>
-      {isLoading ? <LoadingPage /> : <SearchItems />}
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  headerContainer: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-  searchIcon: {
-    backgroundColor: '#BEBEBE',
-    borderBottomLeftRadius: 10,
-    borderTopLeftRadius: 10,
-  },
-  inputContainer: {
-    backgroundColor: '#BEBEBE',
-    justifyContent: 'space-around',
-    borderTopRightRadius: 10,
-    borderBottomRightRadius: 10,
-    width: '55%',
-    height: '80%',
-  },
-  inputText: {
-    fontSize: 14,
-    color: 'black',
-  },
-});
- */
